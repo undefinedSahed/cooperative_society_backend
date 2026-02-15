c9819159bae29d3fbe687ef9045e373661f8c8d1
undefinedSahed <sahedrahmanshakil@gmail.com>
2026-02-15 14:33:11 +0600

    feat: added user module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+
+    constructor(private readonly userService: UserService) { }
+
+    @Post('register')
+    async create() {
+        return "Bla!"
+    }
+}
